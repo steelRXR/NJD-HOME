bd60ed59a2ca42c4d53f65809f33952e0486a989
klimson <klimson@gitlab.com>
2018-11-25 14:29:00 +0100

    Add home loader

---
+++ src/main/java/HomeLoaderImpl.java
@@ -0,0 +1,8 @@
+import models.Home;
+
+public class HomeLoaderImpl implements HomeLoader {
+    @Override
+    public Home loadHome(String fileName, String separator) {
+        return null;
+    }
+}
